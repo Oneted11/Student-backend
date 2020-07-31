a7c35b5f154dfdc37854d5dd701b0810a38cde01
Oneted11 <muriithited@gmail.com>
2020-07-31 13:50:13 +0300

    add mocha chai and create test folder

---
+++ tests/test.js
@@ -0,0 +1,10 @@
+import chai from "chai";
+import chaiHttp from "chai-http";
+import server from "../server.js";
+//chai config
+chai.use(chaiHttp);
+chai.should();
+
+describe("getting students",function(){
+    
+})
